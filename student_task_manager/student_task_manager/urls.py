f6958c6554fc3cb26fe864a4a0edb6e47f794bef
ialishbaimran30 <i.alishbaimran30@gmail.com>
2026-07-14 10:11:58 +0500

    Add Django project

--- student_task_manager/student_task_manager/urls.py
+++ student_task_manager/student_task_manager/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path("test/", test),
     path("tasks/", include("tasks.urls")),
     path("api/token/",TokenObtainPairView.as_view(),name="token_obtain_pair"),
-    path("api/token/refresh",TokenRefreshView.as_view(),name="token_refresh"),
+    path("api/token/refresh/",TokenRefreshView.as_view(),name="token_refresh"),
     path("api-auth/",include("rest_framework.urls")),
 
 ]
